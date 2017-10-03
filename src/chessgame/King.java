ecec409a270eecf68e3cc4e37af28dd7043c59fd
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2017-10-03 22:14:59 +0200

    Added King Script

---
+++ src/chessgame/King.java
@@ -0,0 +1,33 @@
+package chessgame;
+
+public class King extends Piece{
+        King(String s){
+            super(s);
+        }
+        @Override
+        public boolean canMove(Tile c, Tile t){         // needs adjusyments
+            int CI = c.getY()/80;
+            int CJ = c.getX()/80;
+            
+            int TI = t.getY()/80;
+            int TJ = t.getX()/80;
+            
+            if(TI == CI && TJ == CJ+1)
+                return true;
+            if(TI == CI && TJ == CJ-1)
+                return true;
+            
+            if(TI == CI-1 && TJ == CJ)
+                return true;
+            if(TI == CI-1 && TJ == CJ-1)
+                return true;
+            if(TI == CI-1 && TJ == CJ+1)
+                return true;
+            
+            if(TI == CI+1 && TJ == CJ)
+                return true;
+            if(TI == CI+1 && TJ == CJ+1)
+                return true;
+            return TI == CI+1 && TJ == CJ-1;
+        }
+    }
