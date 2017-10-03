d8fd3f64d977d5979c282a5fcaaa9487860ab8f0
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2017-10-04 01:36:32 +0200

    tiny tweaks

--- src/chessgame/WelcomePage.java
+++ src/chessgame/WelcomePage.java
@@ -1,36 +1,19 @@
 package chessgame;
 
-import java.awt.Graphics;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class WelcomePage extends JPanel{	
+public class WelcomePage {	
     private int timer = 1500;
     private int timer2 = 700;
     private int timer3 = 1500;
     
-    private ImageIcon gameLoading = new ImageIcon("gameLoading.png");
-    private ImageIcon logo = new ImageIcon("Logo.png");
-    private ImageIcon Template = new ImageIcon("Template.png");
-    private ImageIcon pressAnyKey = new ImageIcon("pressAnyKey.png");
+    private JPanel welcomePage = new JPanel();
 
     public WelcomePage() {
         while(timer2>0) {
             
             timer2--;
     	}
-        while(timer>0) {
-            
-            timer--;
-        }
-        while(timer3>0) {
-            
-            timer3--;
-        }
     }
-    
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-    }
+
 }
