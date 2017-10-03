4342a11f261167dba548f9084a1f286fd392ddae
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2017-10-03 22:15:12 +0200

    Added Knight Script

---
+++ src/chessgame/Knight.java
@@ -0,0 +1,34 @@
+package chessgame;
+
+public class Knight extends Piece{
+        Knight(String s){
+            super(s);
+        }
+        @Override
+        public boolean canMove(Tile c, Tile t){
+            int CI = c.getY()/80;
+            int CJ = c.getX()/80;
+            
+            int TI = t.getY()/80;
+            int TJ = t.getX()/80;
+            
+            if(TI == CI-2 && TJ == CJ-1)
+                return true;
+            if(TI == CI-2 && TJ == CJ+1)
+                return true;
+            
+            if(TI == CI+2 && TJ == CJ-1)
+                return true;
+            if(TI == CI+2 && TJ == CJ+1)
+                return true;
+            
+            if(TI == CI-1 && TJ == CJ-2)
+                return true;
+            if(TI == CI+1 && TJ == CJ-2)
+                return true;
+            
+            if(TI == CI-1 && TJ == CJ+2)
+                return true;
+            return TI == CI+1 && TJ == CJ+2;
+        }
+    }
