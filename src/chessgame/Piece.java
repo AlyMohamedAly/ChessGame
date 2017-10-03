92cddb40bf379b4ffd6edcf6e2bfb93a4fc2c35c
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2017-10-03 22:15:40 +0200

    Added Piece Script

---
+++ src/chessgame/Piece.java
@@ -0,0 +1,21 @@
+package chessgame;
+
+import javax.swing.JLabel;
+
+public abstract class Piece extends JLabel {
+    private String Name;
+    
+    Piece(String name){
+        Name = name;
+    }
+    
+    String getColor(){
+        return Name.substring(0, 5);
+    }
+    
+    String GetType(){
+        return Name;
+    }
+    
+    public abstract boolean canMove(Tile c, Tile t);
+}
